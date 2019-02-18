e1d29f772984a5c617791b8587e9409184bd7070
shuochen <shuo.chen@doverfs.com>
2019-02-18 15:46:44 +0800

    update

--- serial.js
+++ serial.js
@@ -1,15 +1,23 @@
 var SerialPort = require('serialport');
 var port = new SerialPort('COM5');
 
-var senddata = [0x01, 0x02];
+//发hex
+var senddata = [0x01,0x02];
+//发字符串
+//senddata = 'test data';
 
-port.on('open', function () {
+function writeport()
+{
     port.write(senddata, function (err) {
         if (err) {
             return console.log('Error on write: ', err.message);
         }
-        console.log('message written');
+        console.log('send: ' + senddata);
     });
+}
+
+port.on('open', function () {
+    writeport();
 });
 
 // open errors will be emitted as an error event
@@ -18,10 +26,14 @@ port.on('error', function (err) {
 })
 
 setInterval(function () {
-    port.write(senddata, function (err) {
-        if (err) {
-            return console.log('Error on write: ', err.message);
-        }
-        console.log('message written');
-    });
+    writeport();
 }, 5000);
+
+
+port.on('data', function (data) {
+    //收hex
+    console.log('recv: ' + data.toString('hex'));
+    //收字符串
+    //console.log('recv: ' + data.toString('ascii'));
+  });
+
